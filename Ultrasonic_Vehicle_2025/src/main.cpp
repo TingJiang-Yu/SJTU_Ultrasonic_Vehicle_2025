976e470daefbf12999b8198dd20372185273136e
TingJiang_Yu <longlong51226@qq.com>
2025-12-22 18:46:08 +0800

    微调

--- Ultrasonic_Vehicle_2025/src/main.cpp
+++ Ultrasonic_Vehicle_2025/src/main.cpp
@@ -2,17 +2,14 @@
 #include "main_controller.h"
 
 main_controller ctrl;
-motor_controller motor_test;
 
 void setup() 
 {
     Serial.begin(SERIAL_BAUDRATE);
-    motor_test.init();
-    // ctrl.init();
+    ctrl.init();
 }
 
 void loop() 
 {
-    motor_test.setTurn(30);
-    // ctrl.update();
+    ctrl.update();
 }
